fcdcf49fa5cf1ab8c4b30733a93dcf9294f8a692
IKillerwolfI <killerwolf.no1@gmail.com>
2018-12-12 09:14:17 +0100

    Code setup
    -Code for default parts and sights
    -CAFCW code

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -1,13 +1,13 @@
 ------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------
 --                                                                                      --
---                          ||       |||||||   	   ||||   ||||||             			--
---                         ||||      ||    ||     || ||   ||             				--
---                        ||  ||     ||   ||         ||   ||                           	--
---                       ||||||||    ||||||          ||   |||||                         --
---                      ||      ||   ||   ||         ||       ||                        --
---                     ||        ||  ||    ||        ||       ||					    --
---                    ||          || ||     ||    ||||||| |||||  						--
+--                          ||       |||||||   	   ||||||               				--
+--                         ||||      ||    ||     ||    ||                				--
+--                        ||  ||     ||   ||      ||    ||                             	--
+--                       ||||||||    ||||||        |||||||                            	--
+--                      ||      ||   ||   ||            ||                              --
+--                     ||        ||  ||    ||           ||       					    --
+--                    ||          || ||     ||     ||||||  								--
 --                                                                                      --
 ------------------------------------------------------------------------------------------
 --																						--
@@ -25,9 +25,9 @@
 ------------------------------------------------------------------------------------------
 
 
----------------------------------------------
-----<A><R><1><5> -Functions- <A><R><1><5>----
----------------------------------------------
+---------------------------------------
+----<A><R><9> -Functions- <A><R><9>----
+---------------------------------------
 
 --- Forbid Setup ---
 function WeaponFactoryTweakData:arpack_setup_forbid( mod_one, mod_two )
@@ -102,62 +102,38 @@ function WeaponFactoryTweakData:ar15_write_error( func, cause, error_id, value  
 		-- fti = failed to index
 		if cause == "fti" and value then
 			if error_id == "part" then
-				log("AR15_ERROR: (" .. func .. ")Attempt to index Part ID: '" .. value .. "' (a nil value)")
+				log("AR9_ERROR: (" .. func .. ")Attempt to index Part ID: '" .. value .. "' (a nil value)")
 			elseif error_id == "wpn" then
-				log("AR15_ERROR: (" .. func .. ")Attempt to index Weapon ID: '" .. value .. "' (a nil value)")
+				log("AR9_ERROR: (" .. func .. ")Attempt to index Weapon ID: '" .. value .. "' (a nil value)")
 			elseif error_id == "mod_type" then
-				log("AR15_ERROR: (" .. func .. ")Attempt to index mod_type: '" .. value .. "' (a nil value)")
+				log("AR9_ERROR: (" .. func .. ")Attempt to index mod_type: '" .. value .. "' (a nil value)")
 			end
 		end
 	end
 end 
 
-Hooks:PostHook( WeaponFactoryTweakData, "init", "AR15ModInit", function(self)
+Hooks:PostHook( WeaponFactoryTweakData, "init", "AR9ModInit", function(self)
 
-------------------------------------------
-----<A><R><1><5> -Arrays- <A><R><1><5>----
-------------------------------------------
+------------------------------------
+----<A><R><9> -Arrays- <A><R><9>----
+------------------------------------
 
 --- Stocks ---
-local all_ar15stock = {
-	"wpn_fps_ass_stoy_556_st_m16"
-}
+
 --- Handguard  ---
 -- Default length --
-local all_ar15handguard = {
-	"wpn_fps_ass_stoy_556_hg_ar15",
-	"wpn_fps_ass_stoy_556_hg_m16",
-	"wpn_fps_ass_stoy_556_hg_m16a2",
-	"wpn_fps_ass_stoy_556_hg_m607",
-	"wpn_fps_ass_stoy_556_hg_m4",
-	"wpn_fps_ass_stoy_556_hg_m4a1"
-}
+
 
 --- Barrel ---
 -- Default length --
-local all_ar15barrel = {
-	"wpn_fps_ass_stoy_556_ba_ar15",
-	"wpn_fps_ass_stoy_556_ba_m16a1",
-	"wpn_fps_ass_stoy_556_ba_m16a2",
-	"wpn_fps_ass_stoy_556_ba_car15",
-	"wpn_fps_ass_stoy_556_ba_m733",
-	"wpn_fps_ass_stoy_556_ba_m4",
-	"wpn_fps_ass_stoy_556_ba_m4_short"
-}
 
 --- Front Sights ---
-local all_ar15fo = {
-	"wpn_fps_ass_stoy_556_fo_ak9"
-}
+
 --- Gas Blocks ---
-local all_ar15gb = {
-	"wpn_fps_ass_stoy_556_gb_ar15",
-	"wpn_fps_ass_stoy_556_gb_m16a1",
-	"wpn_fps_ass_stoy_556_gb_m16a4"
-}
+
 
 --- Barrel Extensions --
-local all_ar15ext = {
+local all_ar9ext = {
 	"wpn_fps_upg_ns_ass_smg_large",
 	"wpn_fps_upg_ns_ass_smg_medium",
 	"wpn_fps_upg_ns_ass_smg_small",
@@ -169,15 +145,12 @@ local all_ar15ext = {
 	"wpn_fps_upg_ass_ns_linear",
 	"wpn_fps_upg_ass_ns_surefire",
 	"wpn_fps_upg_ass_ns_battle",
-	"wpn_fps_ass_stoy_556_md_dummy",
-	"wpn_fps_ass_stoy_556_md_ar15",
-	"wpn_fps_ass_stoy_556_md_m16a1",
-	"wpn_fps_ass_stoy_556_md_m16a2",
-	"wpn_fps_ass_stoy_556_md_car15"
+	"wpn_fps_smg_stoy_919_md_dummy",
+	"wpn_fps_smg_stoy_919_md_r0635"
 }
 
 --- Gadgets (Flashlights/Lasers) ---
-local all_ar15fl = {
+local all_ar9fl = {
 	"wpn_fps_upg_fl_ass_smg_sho_peqbox",
 	"wpn_fps_upg_fl_ass_smg_sho_surefire",
 	"wpn_fps_upg_fl_ass_peq15",
@@ -187,7 +160,7 @@ local all_ar15fl = {
 }
 
 --- Sights ---
-local all_ar15sight = {
+local all_ar9sight = {
 	"wpn_fps_upg_o_specter",
 	"wpn_fps_upg_o_aimpoint",
 	"wpn_fps_upg_o_aimpoint_2",
@@ -221,15 +194,12 @@ local nonacog_arsight = {
 
 
 --- Grips ---
-local all_ar15grips = {
-	"wpn_fps_ass_stoy_556_pg_m16"
-}
 
 --- Compatibility Array Insertion ---
 
------------------------------------------
-----<A><R><1><5> -CAFCW- <A><R><1><5>----
------------------------------------------
+-----------------------------------
+----<A><R><9> -CAFCW- <A><R><9>----
+-----------------------------------
 if BeardLib.Utils:FindMod("gsup") then
 	local AR15SneakySuppressorPack = {
 		"wpn_fps_ass_ns_g_sup3",
@@ -237,105 +207,86 @@ if BeardLib.Utils:FindMod("gsup") then
 		"wpn_fps_ass_ns_g_sup5"
 	}
 	for id, part_id in pairs(AR15SneakySuppressorPack) do
-		table.insert(self.wpn_fps_ass_stoy_556.uses_parts, part_id)
-		table.insert(all_ar15ext, part_id)
+		table.insert(self.wpn_fps_smg_stoy_919.uses_parts, part_id)
+		table.insert(all_ar9ext, part_id)
 	end
 end
 
 if BeardLib.Utils:FindMod("Tromix Barrel-Ext") then
-	table.insert(self.wpn_fps_ass_stoy_556.uses_parts, "wpn_fps_upg_ns_ass_smg_tromix")	
-	table.insert(all_ar15ext, "wpn_fps_upg_ns_ass_smg_tromix")
+	table.insert(self.wpn_fps_smg_stoy_919.uses_parts, "wpn_fps_upg_ns_ass_smg_tromix")	
+	table.insert(all_ar9ext, "wpn_fps_upg_ns_ass_smg_tromix")
 end
 
 if BeardLib.Utils:FindMod("M82 Muzzle Brake") then
-	table.insert(all_ar15ext, "wpn_fps_upg_ns_m82")
-	table.insert(all_ar15ext, "wpn_fps_upg_ns_aw50")
+	table.insert(all_ar9ext, "wpn_fps_upg_ns_m82")
+	table.insert(all_ar9ext, "wpn_fps_upg_ns_aw50")
 end
 
 
---------------------------------------------
-----<A><R><1><5> -Override- <A><R><1><5>----
---------------------------------------------
+--------------------------------------
+----<A><R><9> -Override- <A><R><9>----
+--------------------------------------
 
 --- Barrels ---
-for id, ba_id in pairs(all_ar15barrel) do
-	self:arpack_check_override( "part", ba_id )
-end
+
 --- Handguard ---
-for id, hg_id in pairs(all_ar15handguard) do
-	self:arpack_check_override( "part", hg_id )
-end
+
 --FL overrides--
-self.parts.wpn_fps_ass_stoy_556_hg_m4a1.override.wpn_fps_addon_ris = {unit="units/payday2/weapons/wpn_upg_dummy/wpn_upg_dummy"}
-for i, fl_id in pairs(all_ar15fl) do
-	if fl_id ~= "wpn_fps_addon_ris" then
-		self.parts.wpn_fps_ass_stoy_556_hg_m4a1.override[fl_id] = {a_obj="a_fl_m4a1"}
-	end
-end
+--self.parts.wpn_fps_ass_stoy_556_hg_m4a1.override.wpn_fps_addon_ris = {unit="units/payday2/weapons/wpn_upg_dummy/wpn_upg_dummy"}
+--for i, fl_id in pairs(all_ar15fl) do
+--	if fl_id ~= "wpn_fps_addon_ris" then
+--		self.parts.wpn_fps_ass_stoy_556_hg_m4a1.override[fl_id] = {a_obj="a_fl_m4a1"}
+--	end
+--end
 --GB override--
-for id, gb_id in pairs(all_ar15gb) do
-	self.parts.wpn_fps_ass_stoy_556_hg_m607.override[gb_id] = {a_obj="a_gb_car15"}
-	self.parts.wpn_fps_ass_stoy_556_hg_m4.override[gb_id] 	= {a_obj="a_gb_car15"}
-	self.parts.wpn_fps_ass_stoy_556_hg_m4a1.override[gb_id] = {a_obj="a_gb_car15"}
-end
+--for id, gb_id in pairs(all_ar15gb) do
+--	self.parts.wpn_fps_ass_stoy_556_hg_m4a1.override[gb_id] = {a_obj="a_gb_car15"}
+--end
 --MD override--
-for id, ext_id in pairs(all_ar15ext) do
-	if ext_id ~= "wpn_fps_ass_stoy_556_md_dummy" then
-		self.parts.wpn_fps_ass_stoy_556_ba_car15.override[ext_id] = {a_obj="a_ns_car15"}
-		self.parts.wpn_fps_ass_stoy_556_ba_m733.override[ext_id]  = {a_obj="a_ns_m733"}
-		self.parts.wpn_fps_ass_stoy_556_ba_m4.override[ext_id] 	  = {a_obj="a_ns_m4"}
-	end
-end
+
 --- Lowers/Carry Handle ---
 --O override--
-for i, o_id in pairs(all_ar15sight) do
-	self.parts.wpn_fps_ass_stoy_556_ur_m16a3.override[o_id] = {a_obj="a_o_lower", stance_mod={wpn_fps_ass_stoy_556 = {translation = Vector3(0, 5, -0.65)}}}
-	self.parts.wpn_fps_ass_stoy_556_ca_m16a3.override.wpn_fps_ass_stoy_556_ur_m16a3 = {override = {o_id = {}}}
-	self.parts.wpn_fps_ass_stoy_556_ur_m16a4.override[o_id] = {a_obj="a_o_lower", stance_mod={wpn_fps_ass_stoy_556 = {translation = Vector3(0, 5, -0.65)}}}
-	self.parts.wpn_fps_ass_stoy_556_ur_m16a4.override.wpn_fps_ass_stoy_556_ur_m16a3 = {override = {o_id = {}}}
+
+----------------------------------
+----<A><R><9> -Adds- <A><R><9>----
+----------------------------------
+for i, o_id in pairs(all_ar9sight) do
+	self.wpn_fps_smg_stoy_919.adds[o_id] = {"wpn_fps_smg_stoy_919_sm_base","wpn_fps_smg_stoy_919_sm_knob"}
 end
-----------------------------------------
-----<A><R><1><5> -Adds- <A><R><1><5>----
-----------------------------------------
-for i, o_id in pairs(all_ar15sight) do
-	self.wpn_fps_ass_stoy_556.adds[o_id] = {"wpn_fps_ass_stoy_556_sm_base","wpn_fps_ass_stoy_556_sm_knob"}
+-------------------------------------
+----<A><R><9> -Forbids- <A><R><9>----
+-------------------------------------
+for i, fl_id in pairs(all_ar9fl) do
+	table.insert(self.parts.wpn_fps_smg_stoy_919_hg_r0635.forbids, fl_id)
 end
--------------------------------------------
-----<A><R><1><5> -Forbids- <A><R><1><5>----
--------------------------------------------
-for i, fl_id in pairs(all_ar15fl) do
-	table.insert(self.parts.wpn_fps_ass_stoy_556_hg_m16.forbids, fl_id)
-	table.insert(self.parts.wpn_fps_ass_stoy_556_hg_ar15.forbids, fl_id)
-	table.insert(self.parts.wpn_fps_ass_stoy_556_hg_m607.forbids, fl_id)
-end
-------------------------------------------
-----<A><R><1><5> -Stance- <A><R><1><5>----
-------------------------------------------
+------------------------------------
+----<A><R><9> -Stance- <A><R><9>----
+------------------------------------
 
 --- Vanilla sights ---
 -- non-acoc, default sights --
 for i, o_id in pairs(nonacog_arsight) do
-	self.parts[o_id].stance_mod.wpn_fps_ass_stoy_556 = {translation = Vector3(0, 7, -5.05)}
+	self.parts[o_id].stance_mod.wpn_fps_smg_stoy_919 = {translation = Vector3(0, 7, -5.05)}
 end
 -- other sights --
-self.parts.wpn_fps_upg_o_acog.stance_mod.wpn_fps_ass_stoy_556 			  = {translation = Vector3(0, 5, -5.05)}
-self.parts.wpn_fps_upg_o_45rds.stance_mod.wpn_fps_ass_stoy_556 		   	  = deep_clone(self.parts.wpn_fps_upg_o_45rds.stance_mod.wpn_fps_ass_flint)
-self.parts.wpn_fps_upg_o_xpsg33_magnifier.stance_mod.wpn_fps_ass_stoy_556 = deep_clone(self.parts.wpn_fps_upg_o_xpsg33_magnifier.stance_mod.wpn_fps_ass_flint)
-self.parts.wpn_fps_upg_o_45rds_v2.stance_mod.wpn_fps_ass_stoy_556 		  = deep_clone(self.parts.wpn_fps_upg_o_45rds_v2.stance_mod.wpn_fps_ass_flint)
+self.parts.wpn_fps_upg_o_acog.stance_mod.wpn_fps_smg_stoy_919 			  = {translation = Vector3(0, 5, -5.05)}
+self.parts.wpn_fps_upg_o_45rds.stance_mod.wpn_fps_smg_stoy_919 		   	  = deep_clone(self.parts.wpn_fps_upg_o_45rds.stance_mod.wpn_fps_ass_flint)
+self.parts.wpn_fps_upg_o_xpsg33_magnifier.stance_mod.wpn_fps_smg_stoy_919 = deep_clone(self.parts.wpn_fps_upg_o_xpsg33_magnifier.stance_mod.wpn_fps_ass_flint)
+self.parts.wpn_fps_upg_o_45rds_v2.stance_mod.wpn_fps_smg_stoy_919 		  = deep_clone(self.parts.wpn_fps_upg_o_45rds_v2.stance_mod.wpn_fps_ass_flint)
 
-------------------------------------------
-----<A><R><1><5> -Tweaks- <A><R><1><5>----
-------------------------------------------
+------------------------------------
+----<A><R><9> -Tweaks- <A><R><9>----
+------------------------------------
 
 --- Barrel Extensions ---
-self.parts.wpn_fps_ass_stoy_556_md_dummy.pcs = nil
+self.parts.wpn_fps_smg_stoy_919_md_dummy.pcs = nil
 
-------------------------------------------
-----<A><R><1><5> -CLONES- <A><R><1><5>----
-------------------------------------------
+------------------------------------
+----<A><R><9> -CLONES- <A><R><9>----
+------------------------------------
 
----------------------------------------
-----<A><R><1><5> -WIP- <A><R><1><5>----
----------------------------------------
+---------------------------------
+----<A><R><9> -WIP- <A><R><9>----
+---------------------------------
 
 end )
--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -1,82 +1,82 @@
-Hooks:PostHook( WeaponTweakData, "init", "AR15ModInit", function(self)
+Hooks:PostHook( WeaponTweakData, "init", "AR9ModInit", function(self)
 
-if ( self.stoy_556 ) then
+if ( self.stoy_919 ) then
 
 ---- CAP aka YEEET ----	
 
 --- Base stuff ---	
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_ns", 
         base_a_obj = "a_ns", 
-        position = Vector3( 0, 4.1, -0.3 ),
+        position = Vector3( 0, -19.2, -0.35 ),
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_fl", 
         base_a_obj = "a_fl", 
         position = Vector3( 0.6, 0, -0.3 ),
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_gl", 
         base_a_obj = "a_fl", 
         position = Vector3( 0, 0, 0 ),
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_o", 
         base_a_obj = "a_o", 
         position = Vector3( 0, -1.5, 3.9 ),
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_os", 
         base_a_obj = "a_os", 
         position = Vector3( 0, 0, 0 ),
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
         name = "a_bolt_ar15",
         base_a_obj = "a_body",
-        part_attach_data = {{"wpn_fps_ass_stoy_556_ch_ar15"},"g_charginghandle"},
+        part_attach_data = {{"wpn_fps_ass_stoy_919_ch_ar15"},"g_charginghandle"},
         position = Vector3(0, 0, 0),
         rotation = RotationCAP(0, 0, 0)
     })
 --- Gas Block ---
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_gb_car15", 
         base_a_obj = "a_body", 
         position = Vector3( 0, -12.75, 0 ),
         rotation = Rotation( 0, 0, 0 ) 
 	})
 --- MDs ---
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_ns_m4", 
         base_a_obj = "a_ns", 
         position = Vector3( 0, -8.65, -0.3 ),
         rotation = Rotation( 0, 0, 0 )
 	})
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_ns_car15", 
         base_a_obj = "a_ns", 
         position = Vector3( 0, -18.75, -0.3 ),
         rotation = Rotation( 0, 0, 0 )
 	})
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_ns_m733", 
         base_a_obj = "a_ns", 
         position = Vector3( 0, -15.15, -0.3 ),
         rotation = Rotation( 0, 0, 0 )
 	})
 --- Sights ---
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_o_lower", 
         base_a_obj = "a_o", 
         position = Vector3( 0, -1.5, -0.5 ),
         rotation = Rotation( 0, 0, 0 ) 
 	})
 --- Gadgets ---
-	self:SetupAttachmentPoint("stoy_556", {
+	self:SetupAttachmentPoint("stoy_919", {
 		name = "a_fl_m4a1", 
         base_a_obj = "a_fl", 
         position = Vector3( -0.15, 0, -0.3 ),
