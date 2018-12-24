e0bbaf744c27a2e1e743ae87097475c41e7efec1
IKillerwolfI <killerwolf.no1@gmail.com>
2018-12-24 10:52:41 +0100

    Icon setup / Cafcw

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -221,6 +221,59 @@ end
 -----------------------------------
 ----<A><R><9> -CAFCW- <A><R><9>----
 -----------------------------------
+if BeardLib.Utils:FindMod("Custom attachments for custom weapons") then
+	local cafcwsights_ar9 = {
+		"wpn_fps_upg_o_acog_rmr",
+		"wpn_fps_upg_o_gitsch",
+		"wpn_fps_upg_o_aog",
+		"wpn_fps_upg_o_rmr_riser",
+		"wpn_fps_upg_o_eotech552",
+		"wpn_fps_upg_o_po4",
+		"wpn_fps_upg_o_st10",
+		"wpn_fps_upg_o_susat",
+		"wpn_fps_upg_o_okp7",
+		"wpn_fps_upg_o_visionking",
+		"wpn_fps_upg_o_compm2",
+		"wpn_fps_upg_o_kobra",
+		"wpn_fps_upg_o_compm4s",
+		"wpn_fps_upg_o_m145",
+		"wpn_fps_upg_o_pkas",
+		"wpn_fps_upg_o_coyote",
+		"wpn_fps_upg_o_hologram",
+		"wpn_fps_upg_o_zeiss",
+		"wpn_fps_upg_o_hd33",
+		"wpn_fps_upg_o_prismatic",
+		"wpn_fps_upg_o_srs",
+		"wpn_fps_upg_o_hcog",
+		"wpn_fps_upg_o_reflexholo",
+		"wpn_fps_upg_o_elo",
+		"wpn_fps_upg_o_kemper",
+		"wpn_fps_upg_o_mepro",
+		"wpn_fps_upg_o_rusak",
+		"wpn_fps_upg_o_horzine",
+		"wpn_fps_upg_o_pka",
+		"wpn_fps_upg_o_anpas13d"
+	}
+	local cafcwgadgets_ar9 = { 
+		"wpn_fps_upg_fl_ass_spotter",
+		"wpn_fps_upg_fl_wml",
+		"wpn_fps_upg_fl_anpeq2",
+		"wpn_fps_upg_fl_dbal_d2",
+		"wpn_fps_upg_fl_m600p",
+		"wpn_fps_upg_fl_utg"
+	}
+	for id, part_id in pairs(cafcwsights_ar9) do
+		if self.parts[part_id] then
+			table.insert(all_ar9sight, part_id)
+		end
+	end
+	for id, part_id in pairs(cafcwgadgets_ar9) do
+		if self.parts[part_id] then
+			table.insert(all_ar9fl, part_id)
+		end
+	end
+end
+
 if BeardLib.Utils:FindMod("gsup") then
 	local AR15SneakySuppressorPack = {
 		"wpn_fps_ass_ns_g_sup3",
